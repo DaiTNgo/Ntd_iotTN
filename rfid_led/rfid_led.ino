0d0032823cf9a87d3172f68223f2286634b8f545
daingo <daingo7211@gmail.com>
2022-04-27 16:27:57 +0700

    complete function

--- rfid_led/rfid_led.ino
+++ rfid_led/rfid_led.ino
@@ -1,40 +1,21 @@
 #include <SPI.h>
-#include <MFRC522.h> // thu vien "RFID".
+#include <MFRC522.h>
+#include "ACS712.h"
+
 #define SS_PIN 10
 #define RST_PIN 9
 
-#include "ACS712.h"
-
-// Cảm biến 30A, kết nối chân A0
-// ACS712_05B
-// ACS712_20A
-// ACS712_30A
-float tong;
-float I;
-float I_TB;
-float mA;
-float p;
+float tong = 0;
+float I = 0;
+float I_TB = 0;
+float mA = 0;
+float p = 0;
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 unsigned long uidDec, uidDecTemp; // hien thi so UID dang thap phan
 
-const int LED1 = 2; // LED do
-const int LED2 = 3; // LED xanh
-
-int period = 1000;
-unsigned long time_now = 0;
-
-unsigned long ticketNumber;
-
-/**
-  1. Khi người dùng quẹt thẻ, xác định được người dùng => mở ổ điện
-   uid của người dùng sẽ được lưu vào đâu ???
-   2. Khi người dùng rút sạc.
-   sau 10s ổ sạc sẽ tắt, gửi dữ liệu của ổ sạc vừa tắt đó lên server với uid của người vừa sạc
-   3. làm sao biết được ổ nào không còn người sạc nữa
-   mình sẽ dựa vào dòng điện đọc được mà phán doán xem có còn người sạc ổ đó hay không.
-
-*/
+int period = 15000;
+int thresholdCurrent = 100;
 
 class User
 {
@@ -54,7 +35,7 @@ public:
         lastTime = _lastTime;
     }
 };
-
+// TODO:Array User
 User user_0(0, 0, 0, 0, 0);
 User user_1(1, 0, 0, 0, 0);
 User user_2(2, 0, 0, 0, 0);
@@ -62,8 +43,10 @@ User user_3(3, 0, 0, 0, 0);
 User user_4(4, 0, 0, 0, 0);
 
 User Users[5] = {user_0, user_1, user_2, user_3, user_4};
-int powerOutlet[5]; // mảng chứa trạng thái của 5 ổ điện => mức 1 nếu ổ điện đang được sử dụng
-int portPowerOutlet[5] = {22, 23, 24, 25, 26};
+//=====================
+int powerOutlet[5];                            // mảng chứa trạng thái của 5 ổ điện => mức 1 nếu ổ điện đang được sử dụng
+int portPowerOutlet[5] = {22, 23, 24, 25, 26}; // port đọc dòng điện của ổ sạc
+// TODO:Array Sensor: ACS712_05B | ACS712_20A | ACS712_30A
 
 ACS712 sensor_0(ACS712_30A, A0);
 ACS712 sensor_1(ACS712_30A, A1);
@@ -71,10 +54,10 @@ ACS712 sensor_2(ACS712_30A, A2);
 ACS712 sensor_3(ACS712_30A, A3);
 ACS712 sensor_4(ACS712_30A, A4);
 
+ACS712 sensors[5] = {sensor_0, sensor_1, sensor_2, sensor_3, sensor_4};
+//=====================
 void setup()
 {
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
     Serial.begin(9600);
     SPI.begin();
     mfrc522.PCD_Init();
@@ -83,7 +66,11 @@ void setup()
     delay(1000);
     Serial.println("Calibrating...");
     delay(200);
-    sensor.calibrate();
+    for (int i = 0; i < 5; i++)
+    {
+        sensors[i].calibrate();
+    }
+
     Serial.println("Quá trình cân bằng hoàn tất!!!");
 }
 
@@ -93,25 +80,24 @@ void loop()
     if (!mfrc522.PICC_IsNewCardPresent())
     {
         // đợi thời gian để cắm thiết bị sạc =>15s => tắt sau 15s
-        for (int i = 0; < powerOutlet.length; i++)
+        for (int i = 0; i < 5; i++)
         {
             // read dữ liệu port ở vị trí được bật
             if (powerOutlet[i] == 1)
             {
                 for (int i = 0; i < 100; i++)
                 {
-                    I = sensor.getCurrentAC();
+                    I = sensors[i].getCurrentAC();
                     tong = tong + I;
                 }
                 float I_current = tong / 100;
                 I_TB = (I_current + I_TB) / 2;
                 tong = 0;
-                p = 220 * I_TB;
                 mA = I_TB * 1000;
-                // Users[i].breakFirstTime
-                if (I_current * 1000 < 100)
+
+                if (I_current * 1000 < thresholdCurrent)
                 {
-                    if (millis() - Users[i].breakFirstTime < 15000)
+                    if (millis() - Users[i].breakFirstTime < period)
                     {
                         continue;
                     }
@@ -173,7 +159,7 @@ void loop()
     {
         // xem thử port nào chưa sử dụng thì bật lên sử dụng
         // nếu port thứ i thì user_[i]
-        for (int i = 0; i < powerOutlet.length; i++)
+        for (int i = 0; i < 5; i++)
         {
             if (powerOutlet[i] == 0)
             {
