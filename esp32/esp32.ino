ca160c199ffb4a33ffe7e37ec4ae0d02378faba2
daingo_linux <daingo7211@gmail.com>
2022-06-10 16:29:39 +0700

    esp32

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,19 +1,149 @@
-#define RXD2 16
-#define TXD2 17
+#include <Arduino_JSON.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include <Wire.h>
 
-void setup() {
-  // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
-  Serial.begin(115200);
-  //Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("Serial Txd is on pin: "+String(TX));
-  Serial.println("Serial Rxd is on pin: "+String(RX));
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+#define RX 16
+#define TX 17
+
+#define WIFI_SSID "Keobongmay"
+#define WIFI_PASSWORD "135792468"
+#define API_KEY "AIzaSyDCWKOVcceLB-iuc4gMIErYAfGbOxQ7ldc"
+#define DATABASE_URL "https://esp32-firebase-demo-2d982-default-rtdb.asia-southeast1.firebasedatabase.app"
+
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+bool stringComplete = false;
+String inputString = "";
+int lcdColumns = 16;
+int lcdRows = 2;
+
+void ReadSensor();
+void TestFirebase();
+
+void setup()
+{
+
+  Serial.begin(9600);
+  Wire.begin();
+
+  //------UART------
+  //  Serial2.begin(baud-rate, protocol, RX pin, TX pin);
+  Serial2.begin(115200, SERIAL_8N1, RX, TX);
+  Serial.println("Serial Txd is on pin: " + String(TX));
+  Serial.println("Serial Rxd is on pin: " + String(RX));
+  //------UART------
+
+  //------WIFI------
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+  //------WIFI------
+
 }
 
-void loop() { //Choose Serial1 or Serial2 as required
-mySerial.println("gui");
-mySerial.flush(); // doi no truyen het
-  while (Serial2.available()) {
-    Serial.print(char(Serial2.read()));
+void loop()
+{
+//  ReadSensor();
+  TestFirebase();
+}
+
+void ReadSensor() {
+  // put your main code here, to run repeatedly:
+  while (Serial2.available())
+  {
+    char inChar = (char)Serial2.read();
+    inputString += inChar;
+    if (inChar == '\n')
+    {
+      stringComplete = true;
+    }
+    if (stringComplete)
+    {
+      Serial.print("Data nhan: ");
+      Serial.println(inputString);
+
+      JSONVar myObject = JSON.parse(inputString);
+      Serial.print("chuỗi DATA Là:");
+      Serial.println(inputString);
+
+      String current = (String)myObject["current"];
+      String uid = (String)myObject["uid"];
+      String firstTime = (String)myObject["firstTime"];
+      String lastTime = (String)myObject["lastTime"];
+// send data
+
+      inputString = "";
+      stringComplete = false;
+    }
+  }
+}
+void TestFirebase(String current, String uid,String lastTime,String firstTime) {
+  if (Firebase.ready() && signupOK) {
+    if (Firebase.RTDB.setString(&fbdo, "test/current", current)) {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    if (Firebase.RTDB.setString(&fbdo, "test/lastTime",lastTime)) {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    if (Firebase.RTDB.setString(&fbdo, "test/firstTime",firstTime)) {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
   }
 }
--- rfid_led/rfid_led.ino
+++ rfid_led/rfid_led.ino
@@ -1,20 +1,21 @@
+#include <LiquidCrystal_I2C.h>
+
 #include <SPI.h>
 #include <MFRC522.h>
 #include "ACS712.h"
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 #define SS_PIN 10 // SDA
 #define RST_PIN 9
 /**
- * @brief
- * SCK 52
- * MOSI 51
- * MISO 50
- * 3.3 3.3v
- *
- */
+   @brief
+   SCK 52
+   MOSI 51
+   MISO 50
+   3.3 3.3v
+
+*/
 float I = 0;
 float I_TB = 0;
 
@@ -26,7 +27,7 @@ int thresholdCurrent = 100;
 
 class User
 {
-public:
+  public:
     int port;
     int uid;
     unsigned long firstTime;
@@ -35,12 +36,12 @@ public:
     float current;
     User(int _port, int _uid, float _current, unsigned long _breakFirstTime, unsigned long _firstTime, unsigned long _lastTime)
     {
-        port = _port;
-        uid = _uid;
-        breakTime = _breakFirstTime;
-        firstTime = _firstTime;
-        lastTime = _lastTime;
-        current = _current;
+      port = _port;
+      uid = _uid;
+      breakTime = _breakFirstTime;
+      firstTime = _firstTime;
+      lastTime = _lastTime;
+      current = _current;
     }
 };
 // TODO:Array User
@@ -67,187 +68,191 @@ ACS712 sensors[5] = {sensor_0, sensor_1, sensor_2, sensor_3, sensor_4};
 const int numberPort = 1;
 void setup()
 {
-    Serial.begin(9600);
-    Serial.begin(115200);
-    SPI.begin();
-    mfrc522.PCD_Init();
-    Serial.println("Prilozhite kartu / Waiting for card...");
-    Serial.println("Đảm bảo không có dòng điện đi qua cảm biến trong quá trình cân bằng");
-    Serial.println("Calibrating...");
-    for (int i = 0; i < numberPort; i++)
-    {
-        sensors[i].calibrate();
-        delay(1000);
-    }
+  Serial.begin(9600);
+  Serial.begin(115200);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Prilozhite kartu / Waiting for card...");
+  Serial.println("Đảm bảo không có dòng điện đi qua cảm biến trong quá trình cân bằng");
+  Serial.println("Calibrating...");
+  for (int i = 0; i < numberPort; i++)
+  {
+    sensors[i].calibrate();
+    delay(1000);
+  }
 
-    Serial.println("Quá trình cân bằng hoàn tất!!!");
-    // LCD
-    lcd.init();
-    lcd.begin(16, 2);
-    lcd.backlight();
-    lcd.setCursor(0, 0);
-    lcd.print("Xin chao ^^");
-    lcd.setCursor(0, 1);
-    lcd.print("Moi ban quet the");
+  Serial.println("Quá trình cân bằng hoàn tất!!!");
+  // LCD
+  lcd.init();
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Xin chao ^^");
+  lcd.setCursor(0, 1);
+  lcd.print("Moi ban quet the");
 }
 
 void loop()
 {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Xin chao ^^");
+  lcd.setCursor(0, 1);
+  lcd.print("Moi ban quet the");
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    // đợi thời gian để cắm thiết bị sạc =>15s => tắt sau 15s
+    for (int i = 0; i < numberPort; i++)
+    {
+      // read dữ liệu port ở vị trí được bật
+      if (powerOutlet[i] == 1)
+      {
+        float tong = 0;
+        Serial.println("Vào đọc cổng sạc đang mở.");
+        for (int x = 0; x < 100; x++)
+        {
+          I = sensors[i].getCurrentAC();
+          tong = tong + I;
+        }
+        float I_current = tong / 100;
+        Serial.print("Dòng sạc mA hiện tại là: ");
+        Serial.println(I_current * 1000);
+        if (I_current * 1000 < thresholdCurrent)
+        {
+          Serial.print("Threshold Current.");
+          if (millis() - users[i].breakTime < period)
+          {
+            Serial.println("Period.");
+            continue;
+          }
+          else
+          {
+            Serial.println("Đóng cổng sạc sao khi thấy quá thời gian chờ.");
+            users[i].lastTime = millis();
+            //            {
+            //"current" : "fsd",
+            //"time" : "",
+            //"uid" : "",
+            //"lastTime": ""
+            //            };
+
+            // Send Json
+            String sData = "{\"current\":\"" + String(users[i].current) + "\","
+                           + "\"uid\":\"" + String(users[i].uid) + "\","
+                           + "\"lastTime\":\"" + String(users[i].lastTime) + "\","
+                           + "\"firstTime\":\"" + String(users[i].firstTime) + "\"}";
+            Serial2.println(sData);
+            Serial2.flush();
+            powerOutlet[i] = 0;
+            users[i].uid = 0;
+            users[i].breakTime = 0;
+            users[i].firstTime = 0;
+            users[i].lastTime = 0;
+            users[i].current = 0;
+          }
+        }
+        else
+        {
+          users[i].breakTime = millis();
+          if (users[i].firstTime == 0)
+          {
+            users[i].firstTime = millis();
+          }
+          if (users[i].current < I_current)
+          {
+            users[i].current = I_current;
+          }
+
+          Serial.println("Dong sac của thiết bị hiện tại là:");
+          Serial.println(users[i].current * 1000);
+        }
+      }
+    }
+
+    return;
+  }
+  // Doc the
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  uidDec = 0;
+
+  // Hien thi so UID cua the
+  Serial.println("Card UID: ");
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    uidDecTemp = mfrc522.uid.uidByte[i];
+    uidDec = uidDec * 256 + uidDecTemp;
+  }
+  Serial.print(uidDec);
+
+  // Hien thi loai the
+  byte piccType = mfrc522.PICC_GetType(mfrc522.uid.sak); // Tra cuu dinh dang the
+  Serial.println("Tip karty / Card type: ");
+  Serial.print(mfrc522.PICC_GetTypeName(piccType));
+
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_UL)
+  {
+    Serial.println(" Type Card in else.");
+    Serial.print(piccType);
+    Serial.println(" Type Card is not valid");
+
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Xin chao ^^");
+    lcd.print("Card isn't valid");
     lcd.setCursor(0, 1);
-    lcd.print("Moi ban quet the");
-    if (!mfrc522.PICC_IsNewCardPresent())
-    {
-        // đợi thời gian để cắm thiết bị sạc =>15s => tắt sau 15s
-        for (int i = 0; i < numberPort; i++)
-        {
-            // read dữ liệu port ở vị trí được bật
-            if (powerOutlet[i] == 1)
-            {
-                float tong = 0;
-                Serial.println("Vào đọc cổng sạc đang mở.");
-                for (int x = 0; x < 100; x++)
-                {
-                    I = sensors[i].getCurrentAC();
-                    tong = tong + I;
-                }
-                float I_current = tong / 100;
-                Serial.print("Dòng sạc mA hiện tại là: ");
-                Serial.println(I_current * 1000);
-                if (I_current * 1000 < thresholdCurrent)
-                {
-                    Serial.print("Threshold Current.");
-                    if (millis() - users[i].breakTime < period)
-                    {
-                        Serial.println("Period.");
-                        continue;
-                    }
-                    else
-                    {
-                        Serial.println("Đóng cổng sạc sao khi thấy quá thời gian chờ.");
-                        users[i].lastTime = millis();
-                        // {
-                        //     "current" : "fsd",
-                        //     "time" : "",
-                        //     "uid" : "",
-                        // };
-
-                        // Send Json
-                        String sData = "{\"current\":\"" + String(users[i].current) + "\"," + "\"time\":\"" + String(users[i].lastTime - users[i].firstTime) + "\"," + "\"uid\":\"" + String(users[i].uid) + "\"}";
-                        Serial2.println(sData);
-                        Serial2.flush();
-                        powerOutlet[i] = 0;
-                        users[i].uid = 0;
-                        users[i].breakTime = 0;
-                        users[i].firstTime = 0;
-                        users[i].lastTime = 0;
-                        users[i].current = 0;
-                    }
-                }
-                else
-                {
-                    users[i].breakTime = millis();
-                    if (users[i].firstTime == 0)
-                    {
-                        users[i].firstTime = millis();
-                    }
-                    if (users[i].current < I_current)
-                    {
-                        users[i].current = I_current;
-                    }
-
-                    Serial.println("Dong sac của thiết bị hiện tại là:");
-                    Serial.println(users[i].current * 1000);
-                }
-            }
-        }
-
-        return;
-    }
-    // Doc the
-    if (!mfrc522.PICC_ReadCardSerial())
-    {
-        return;
-    }
-    uidDec = 0;
-
-    // Hien thi so UID cua the
-    Serial.println("Card UID: ");
-    for (byte i = 0; i < mfrc522.uid.size; i++)
-    {
-        uidDecTemp = mfrc522.uid.uidByte[i];
-        uidDec = uidDec * 256 + uidDecTemp;
-    }
-    Serial.print(uidDec);
-
-    // Hien thi loai the
-    byte piccType = mfrc522.PICC_GetType(mfrc522.uid.sak); // Tra cuu dinh dang the
-    Serial.println("Tip karty / Card type: ");
-    Serial.print(mfrc522.PICC_GetTypeName(piccType));
-
-    if (piccType != MFRC522::PICC_TYPE_MIFARE_UL)
-    {
-        Serial.println(" Type Card in else.");
-        Serial.print(piccType);
-        Serial.println(" Type Card is not valid");
-
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Card isn't valid");
-        lcd.setCursor(0, 1);
-        lcd.print("Sorry, Again.");
-        delay(500);
-        mfrc522.PICC_HaltA();
-        mfrc522.PCD_StopCrypto1();
-        return;
-    }
-
-    if (uidDec == 3258053915 || uidDec == 3005101082)
-    {
-        // xem thử port nào chưa sử dụng thì bật lên sử dụng
-        // nếu port thứ i thì user_[i]
-        Serial.println();
-        Serial.println("Thẻ hợp lệ.");
-        for (int i = 0; i < numberPort; i++)
-        {
-            if (powerOutlet[i] == 0)
-            {
-                lcd.clear();
-                lcd.setCursor(0, 0);
-                lcd.print("Port");
-                lcd.setCursor(6, 0);
-                lcd.print(i);
-                lcd.setCursor(8, 0);
-                lcd.print("Open.");
-                lcd.setCursor(0, 1);
-                lcd.print("You can charge");
-                delay(500);
-                Serial.println("bật cổng sạc.");
-
-                powerOutlet[i] = 1;
-                // bật cổng sạc
-                digitalWrite(portPowerOutlet[i], 1);
-
-                users[i].uid = uidDec;
-                users[i].breakTime = millis();
-                Serial.println("UID của thiết bị đang sạc là:");
-                Serial.println(users[i].uid);
-            }
-        }
-    }
-    else
-    {
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Card isn't Valid.");
-        lcd.setCursor(0, 1);
-        lcd.print("Sorry, Again.");
-        delay(500);
-        Serial.println("Sai thẻ rồi.");
-    }
-
+    lcd.print("Sorry, Again.");
+    delay(500);
     mfrc522.PICC_HaltA();
     mfrc522.PCD_StopCrypto1();
+    return;
+  }
+
+  if (uidDec == 3258053915 || uidDec == 3005101082)
+  {
+    // xem thử port nào chưa sử dụng thì bật lên sử dụng
+    // nếu port thứ i thì user_[i]
+    Serial.println();
+    Serial.println("Thẻ hợp lệ.");
+    for (int i = 0; i < numberPort; i++)
+    {
+      if (powerOutlet[i] == 0)
+      {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Port");
+        lcd.setCursor(6, 0);
+        lcd.print(i);
+        lcd.setCursor(8, 0);
+        lcd.print("Open.");
+        lcd.setCursor(0, 1);
+        lcd.print("You can charge");
+        delay(500);
+        Serial.println("bật cổng sạc.");
+
+        powerOutlet[i] = 1;
+        // bật cổng sạc
+        digitalWrite(portPowerOutlet[i], 1);
+
+        users[i].uid = uidDec;
+        users[i].breakTime = millis();
+        Serial.println("UID của thiết bị đang sạc là:");
+        Serial.println(users[i].uid);
+      }
+    }
+  }
+  else
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Card isn't Valid.");
+    lcd.setCursor(0, 1);
+    lcd.print("Sorry, Again.");
+    delay(500);
+    Serial.println("Sai thẻ rồi.");
+  }
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
 }
